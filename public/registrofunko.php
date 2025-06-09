c8562f0e930c826aba09b136f7a6574efdf813e8
Kevin Castro Trujillo <kecastru@hotmail.com>
2025-06-09 11:31:55 +0200

    Update registrofunko.php

--- public/registrofunko.php
+++ public/registrofunko.php
@@ -85,7 +85,7 @@ unset($_SESSION['registro_mensaje']);
 <!-- Scripts -->
 <script src="js/scriptbtn.js"></script>
 
-<!-- Muestra un mensaje emergente -->
+<!-- Muestra un mensaje  -->
 <?php if ($mensaje): ?>
 <script>
     alert("<?php echo htmlspecialchars($mensaje); ?>");
